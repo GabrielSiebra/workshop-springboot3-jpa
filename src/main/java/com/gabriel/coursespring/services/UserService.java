1022f2f094d8f48c4028142579c9dcc918db9c3c
GabrielSiebra <gabrielsiebra2014@gmail.com>
2025-06-16 19:48:48 -0300

    Exception handling - delete

--- src/main/java/com/gabriel/coursespring/services/UserService.java
+++ src/main/java/com/gabriel/coursespring/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.coursespring.entities.User;
 import com.gabriel.coursespring.repositories.UserRepository;
+import com.gabriel.coursespring.services.exceptions.DatabaseException;
 import com.gabriel.coursespring.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,14 @@ public class UserService{
 	}
 	
 	public void delete (Long id) {
-		repository.deleteById(id);;
+		try{
+			repository.deleteById(id);;
+		} catch (EmptyResultDataAccessException e ){
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update (Long id, User obj) {
