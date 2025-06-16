1d953328d2c61647e381dcfe66787e21ccebe274
GabrielSiebra <gabrielsiebra2014@gmail.com>
2025-06-16 19:53:57 -0300

    Exception handling - update

--- src/main/java/com/gabriel/coursespring/services/UserService.java
+++ src/main/java/com/gabriel/coursespring/services/UserService.java
@@ -14,47 +14,52 @@ import com.gabriel.coursespring.services.exceptions.DatabaseException;
 import com.gabriel.coursespring.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService{
-	
+public class UserService {
+
 	@Autowired
 	private UserRepository repository;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
-		
+
 	}
-	
-	public User insert (User obj) {
+
+	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
-	public void delete (Long id) {
-		try{
-			repository.deleteById(id);;
-		} catch (EmptyResultDataAccessException e ){
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+			;
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-		
+
 	}
-	
+
 	public User update (Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch(RuntimeException e) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id)
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	
+
 }
